aba993f00c8783dcfdc77b64d97802d204e9e288
Ace <ace.gjorgjievski6@gmail.com>
2025-07-22 00:17:13 +0200

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
+import { TaskListComponent } from './views/task-list/task-list.component';
+import { AddEditComponent } from './views/add-edit/add-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'home', component: HomeComponent },
+    { path: 'add',  component: AddEditComponent},
+    { path: '/taskList', component: TaskListComponent },
+
+    { path: 'not-found', component: NotFoundComponent },
+    { path: '**', redirectTo: 'not-found' }
+];
