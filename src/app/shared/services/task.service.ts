4e2df24fd5c678ab3f2e67577f7e9f8318e09f12
Ace <ace.gjorgjievski6@gmail.com>
2025-07-22 08:15:44 +0200

    feat: add crud methods

--- src/app/shared/services/task.service.ts
+++ src/app/shared/services/task.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'api/tasks';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task);
+  }
+
+  deleteTask(id: number): Observable<unknown> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 }
+
