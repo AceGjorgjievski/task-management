e036e1580ecbcb970730372e1412725ebbf80fa1
Ace <ace.gjorgjievski6@gmail.com>
2025-07-22 18:20:45 +0200

    style: add table view for tasks

--- src/app/views/task-list/task-list.component.ts
+++ src/app/views/task-list/task-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from '../../shared/models/task';
 import { TaskService } from '../../shared/services/task.service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-task-list',
@@ -16,11 +17,23 @@ export class TaskListComponent implements OnInit {
   tasks: Task[] = [];
 
   constructor(
-    private taskService: TaskService
+    private taskService: TaskService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.getTasks();
+  }
+
+  editTask(id: number) {
+    this.router.navigate(['/add'], { queryParams: { id } });
+  }
+
+  deleteTask(id: number) {
+    console.log("delete", id);
+    this.taskService.deleteTask(id).subscribe(() => {
+      this.tasks = this.tasks.filter(task => task.id !== id);
+    });
   }
 
   private getTasks() {
