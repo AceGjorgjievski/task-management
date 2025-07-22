6f3dbe0a2b0bd11337eec6b0609ab7f224b6290f
Ace <ace.gjorgjievski6@gmail.com>
2025-07-22 08:51:03 +0200

    feat: get tasks

--- src/app/views/task-list/task-list.component.ts
+++ src/app/views/task-list/task-list.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../shared/models/task';
+import { TaskService } from '../../shared/services/task.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
 
+  tasks: Task[] = [];
+
+  constructor(
+    private taskService: TaskService
+  ) {}
+
+  ngOnInit(): void {
+    this.getTasks();
+  }
+
+  private getTasks() {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
+  }
 }
