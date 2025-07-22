17165239cebe47efabaa97b8eee8fa92ece5b9cd
Ace <ace.gjorgjievski6@gmail.com>
2025-07-22 08:17:14 +0200

    style: change the style of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./core/navbar/navbar.component";
+import { HomeComponent } from './views/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    NavbarComponent,
+    RouterLink,
+    RouterLinkActive,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
