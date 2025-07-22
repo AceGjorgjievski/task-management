8ee7e63b430815f4e7ec744092fa7351c74db9d8
Ace <ace.gjorgjievski6@gmail.com>
2025-07-22 17:14:31 +0200

    feat: update the page title based on current page

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  pageTitle = 'Welcome';
 
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const url = this.router.url;
+
+        if (url.includes('/home')) {
+          this.pageTitle = 'Welcome to Home Page!';
+        } else if (url.includes('/taskList')) {
+          this.pageTitle = 'My Task List';
+        } else {
+          this.pageTitle = 'Task Manager';
+        }
+      });
+  }
 }
