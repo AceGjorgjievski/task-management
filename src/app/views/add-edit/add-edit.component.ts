e0b0cf89be501e3f319435282d526d714edeabac
Ace <ace.gjorgjievski6@gmail.com>
2025-07-22 18:25:30 +0200

    feat: update the addEdit component

--- src/app/views/add-edit/add-edit.component.ts
+++ src/app/views/add-edit/add-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TaskService } from '../../shared/services/task.service';
+import { Task } from '../../shared/models/task';
 
 @Component({
   selector: 'app-add-edit',
@@ -6,6 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './add-edit.component.html',
   styleUrl: './add-edit.component.css'
 })
-export class AddEditComponent {
+export class AddEditComponent implements OnInit{
+  taskId!: number | null;
+  taskToEdit: Task | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe(params => {
+      const idParam = params.get('id');
+      this.taskId = idParam ? +idParam : null;
+
+      if (this.taskId) {
+        this.taskService.getTask(this.taskId).subscribe((task) => {
+          this.taskToEdit = task;
+        })
+      }
+    });
+  }
 }
