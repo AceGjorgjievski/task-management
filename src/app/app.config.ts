06d4516ff819e5e0078644d389133f0731359510
Ace <ace.gjorgjievski6@gmail.com>
2025-07-22 08:50:44 +0200

    fix: move HttpClientInMemoryWebApiModule in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './mock/in-memory-data.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(HttpClientModule,
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+        delay: 100,
+        dataEncapsulation: false
+      })
+    ),
   ]
 };
