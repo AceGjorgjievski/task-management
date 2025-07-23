c611f9c88f4ab48350f86de1bd09197fa5517aca
Ace <ace.gjorgjievski6@gmail.com>
2025-07-23 23:13:55 +0200

    feat: update header component

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,29 +1,44 @@
 import { Component } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, RouterLink, RouterModule } from '@angular/router';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    NgbCollapseModule,
+    RouterLink,
+    RouterModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  pageTitle = 'Welcome';
+  pageTitle = 'Task Manager';
+  isCollapsed = true;
 
-  constructor(private router: Router) {
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.setTitle(this.router.url);
+
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe(() => {
-        const url = this.router.url;
-
-        if (url.includes('/home')) {
-          this.pageTitle = 'Welcome to Home Page!';
-        } else if (url.includes('/taskList')) {
-          this.pageTitle = 'My Task List';
-        } else {
-          this.pageTitle = 'Task Manager';
-        }
+      .subscribe((event: NavigationEnd) => {
+        this.setTitle(event.urlAfterRedirects);
       });
+  }
+
+  private setTitle(url: string) {
+    if (url.includes('/home')) {
+      this.pageTitle = 'Welcome to Home Page!';
+    } else if (url.includes('/taskList')) {
+      this.pageTitle = 'My Task List';
+    } else if (url.includes('/add')) {
+      this.pageTitle = "Add New Task";
+    } else {
+      this.pageTitle = 'Task Manager';
+    }
   }
 }
